15043c447b9b0124e764022a7baf2ba602ddb205
ana monteiro <anamonteiro430@gmail.com>
2022-05-19 22:41:37 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Right from './components/Right/Right';
 import { getWeatherDataForCity } from './api/api';
 import { WeatherResponse } from './interfaces/interfaces';
 import EmptyState from './components/EmptyState/EmptyState';
+import LoadingState from './components/LoadingState/LoadingState';
 
 function App() {
   const [location, setLocation] = useState<string>("Porto")
   const [weather, setWeather] = useState<WeatherResponse | null>(null)
   const [isCelsius, setIsCelsius] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     (async () => {
+      setIsLoading(true)
       const weather = await getWeatherDataForCity(location, isCelsius);
       setWeather(weather);
+      setIsLoading(false)
     })();
   }, [location, isCelsius]);
   
@@ -30,7 +34,7 @@ function App() {
   return (
     <div className="App" >
       <Left handleLocation={handleLocation} location={location} isCelsius={isCelsius} handleUnits={handleUnits} />
-      {weather ? <Right weatherData={weather} isCelsius={isCelsius}/> : <EmptyState />}
+      {isLoading ? <LoadingState /> : weather ? <Right weatherData={weather} isCelsius={isCelsius}/> : <EmptyState />}
     </div>
   );
 }
--- src/components/Right/Additional/Additional.tsx
+++ src/components/Right/Additional/Additional.tsx
@@ -9,18 +9,18 @@ function Additional({sunrise, sunset}: AdditionalProps) {
   return (
     <div className="additional">
       <div className="additional-sun">
-      <div className="additional-sunrise">
-        <Sunrise size={48} />
-        <p>Sunrise</p>
-        <Divider color={'accentColor'} />
-        <p className="additional-time">{moment.unix(sunrise).hours()}:{`0${String(moment.unix(sunrise).minutes())}`.slice(-2)}H</p>
-      </div>
-      <div className="additional-sunset">
-        <Sunset size={48} />
-        <p>Sunset</p>
-        <Divider color={'accentColor'} />
-        <p className="additional-time">{moment.unix(sunset).hours()}:{`0${String(moment.unix(sunset).minutes())}`.slice(-2)}H</p>
-      </div>
+        <div className="additional-sunrise">
+          <Sunrise size={48} />
+          <p>Sunrise</p>
+          <Divider color={'accentColor'} />
+          <p className="additional-time">{moment.unix(sunrise).hours()}:{`0${String(moment.unix(sunrise).minutes())}`.slice(-2)}H</p>
+        </div>
+        <div className="additional-sunset">
+          <Sunset size={48} />
+          <p>Sunset</p>
+          <Divider color={'accentColor'} />
+          <p className="additional-time">{moment.unix(sunset).hours()}:{`0${String(moment.unix(sunset).minutes())}`.slice(-2)}H</p>
+        </div>
     </div>
     </div>
   );
